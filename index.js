a7c317177b42a46b058ba51cc1113fd1e0e70997
divyapalTatva <117799900+divyapalTatva@users.noreply.github.com>
2025-06-30 12:09:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// A simple GET route
+app.get('/', (req, res) => {
+  res.send('Hello from your simple Node.js API! V1.0 from GKE');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}`);
+});
